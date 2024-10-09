8dbf5dff3454af174ed6c35087c67d7d55f33b3e
Карина Печерная <karina.pecherna.03@gmail.com>
2024-10-09 14:10:37 +0300

    s

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab6.3/FileName.cpp"  
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestSort
+{
+    TEST_CLASS(UnitTestSort)
+    {
+    public:
+
+        TEST_METHOD(TestSortFunction)
+        {
+            const int SIZE = 10;
+            int arr[SIZE] = { 9, 3, 7, 5, 8, 8, 5, 7, 4, 11 };
+            int expected[SIZE] = { 3, 4, 5, 5, 7, 7, 8, 8, 9, 11 };
+
+            Sort(arr, SIZE);
+
+            
+            for (int i = 0; i < SIZE; i++) {
+                Assert::AreEqual(expected[i], arr[i]);
+            }
+        }
+
+        
+
+       
+    };
+}
---
+++ lab6.3/FileName.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+void Sort(int* a, const int size) 
+{
+	for (int i = 0; i < size - 1; i++) 
+	{
+	int min = a[i]; 
+		int imin = i; 
+		for (int j = i + 1; j < size; j++)
+			if (min > a[j])
+			{
+				min = a[j];
+				imin = j;
+			}
+		a[imin] = a[i]; 
+		a[i] = min; 
+	}
+}
+void Print(int* a, const int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << setw(4) << a[i];
+	cout << endl;
+}
+int main()
+{
+	const int n = 10;
+	int a[n] = { 9, 3, 7, 5, 8, 8, 5, 7, 4, 11 };
+	Print(a, n);
+	Sort(a, n);
+	Print(a, n);
+	return 0;
+}
